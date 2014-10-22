2aa6cd3f165eaba40ff23ed883e5e4e135dfa69c
matarn534 <matarn534@E29782.edu.linkoping.se>
2014-10-22 09:31:10 +0200

    Första upggiften i php klar.

---
+++ Katter.php
@@ -0,0 +1,28 @@
+
+<?php
+
+        // put your code here
+        ?>
+
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form>
+            <input type="submit" name="visa" value="Visa">
+            <br>
+            Lägg till. Namn:<input type="text" name="namn"><input type="submit" name="add" value="Add">
+            Ta bort. Namn:
+        
+        </form>
+    </body>
+</html>
